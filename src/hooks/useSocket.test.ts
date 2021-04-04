90915a2cec1b1930f6dde90fb219a79e1e212b8e
tylp <azertyzhd@gmail.com>
2021-04-04 21:24:20 +0200

    add sample test

---
+++ src/hooks/useSocket.test.ts
@@ -0,0 +1,45 @@
+import { Server, Socket } from "socket.io";
+import { createServer } from "http";
+import Client from "socket.io-client";
+
+describe("alo", () => {
+    let io;
+    let clientSocket: SocketIOClient.Socket;
+    let serverSocket: Socket;
+
+    beforeAll((done) => {
+        const httpServer = createServer();
+        io = new Server(httpServer);
+        httpServer.listen(() => {
+            const port = httpServer.address().port;
+            clientSocket = new Client(`http://localhost:${port}`);
+            io.on("connection", (socket) => {
+                serverSocket = socket;
+            });
+            clientSocket.on("connect", done);
+        });
+    });
+
+    afterAll(() => {
+        io.close();
+        clientSocket.close();
+    });
+
+    test("should work", (done) => {
+        clientSocket.on("hello", (arg) => {
+            expect(arg).toBe("world");
+            done();
+        });
+        serverSocket.emit("hello", "world");
+    });
+
+    test("should work (with ack)", (done) => {
+        serverSocket.on("hi", (cb) => {
+            cb("hola");
+        });
+        clientSocket.emit("hi", (arg) => {
+            expect(arg).toBe("hola");
+            done();
+        });
+    });
+});
