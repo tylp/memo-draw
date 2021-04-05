c8c043452e87f8ef129ad6ecf3fdab9ba78d6e6b
tylp <azertyzhd@gmail.com>
2021-04-05 15:35:11 +0200

    add eslint

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -4,27 +4,26 @@ import io from 'socket.io-client';
 const socket = io();
 
 interface SocketCallback {
-	(s: SocketIOClient.Socket): void;
+    (s: SocketIOClient.Socket): void;
 }
 
 export default function useSocket(cb?: SocketCallback) {
-	
-	const [activeSocket, setActiveSocket] = useState<SocketIOClient.Socket>(null);
+    const [activeSocket, setActiveSocket] = useState<SocketIOClient.Socket>(null);
 
-	useEffect(() => {
-		// debug("Socket updated", { socket, activeSocket });
-		if (activeSocket || !socket) return;
-		cb && cb(socket);
-		setActiveSocket(socket);
+    useEffect(() => {
+        // debug("Socket updated", { socket, activeSocket });
+        if (activeSocket || !socket) return;
+        cb && cb(socket);
+        setActiveSocket(socket);
 
         /**
          * Cleanup of all socket handlers
          */
-		return function cleanup() {
-			// debug("Running useSocket cleanup", { socket });
-			socket.off("hello-room", cb);
-		};
-	}, [socket]);
+        return function cleanup() {
+            // debug("Running useSocket cleanup", { socket });
+            socket.off("hello-room", cb);
+        };
+    }, [socket]);
 
-	return activeSocket;
+    return activeSocket;
 }
