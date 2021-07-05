12263f923c0d74e904fd1e469b6fc8b415a7af92
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-07-05 22:33:52 +0200

    add colors palette - tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,31 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        'darker-blue': '#004063',
+        'blue': '#006DBB',
+        'light-blue': '#00BAE3',
+        'lighter-blue': '#33C5F5',
+      },
+      black: {
+        'black': '#000000'
+      },
+      grey: {
+        'grey': '#4D4D4D',
+      },
+      pink: {
+        'dark-pink': '#ED1E79',
+        'light-pink': '#FF7BAC',
+      },
+      yellow: {
+        'dark-yellow': '#CCAC0F',
+        'light-yellow': '#FFE500',
+      },
+      white: {
+        'white': '#FFFFFF'
+      }
+    }
   },
   variants: {
     extend: {},
