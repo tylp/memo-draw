2eaf83b4f982a74d90c7f68cb4e5e64fa42caf8f
apl <antoine.plard@safrangroup.com>
2021-07-09 10:43:10 +0200

    add profile selector

--- src/components/home/profileSelector/profileSelector.tsx
+++ src/components/home/profileSelector/profileSelector.tsx
@@ -1,5 +1,6 @@
 import React, {useState } from "react";
 import {SelectButtonSpec} from './profileSelector.spec';
+import { Title } from "../../common";
 
 export function SelectButton(specs: SelectButtonSpec) : JSX.Element {
     return (
@@ -7,20 +8,20 @@ export function SelectButton(specs: SelectButtonSpec) : JSX.Element {
             <p className="text-md text-white-white">{specs.name}</p>
             <button 
                 onClick={specs.onClick} 
-                className="bg-grey-grey hover:bg-yellow-dark-yellow text-gray-800 font-bold py-1 px-1 rounded-full inline-flex items-center transition duration-300"
+                className="bg-blue-200 hover:bg-yellow-dark-yellow text-gray-800 font-bold py-1 px-1 rounded-full inline-flex items-center transition duration-300"
             >
                 {
                     specs.direction === 'left' 
                     ? 
                         <svg viewBox="0 0 32 32" 
-                            className="icon icon-arrow-left w-6 md:w-8 fill-current text-white-white" 
+                            className="icon icon-arrow-left w-8 fill-current text-white-white" 
                             aria-hidden="true"
                         >
                             <path d="M26.025 14.496l-14.286-.001 6.366-6.366L15.979 6 5.975 16.003 15.971 26l2.129-2.129-6.367-6.366h14.29z"/>
                         </svg>
                     : 
                         <svg viewBox="0 0 32 32" 
-                            className="transform rotate-180 icon icon-arrow-left w-6 md:w-8 fill-current text-white-white" 
+                            className="transform rotate-180 icon icon-arrow-left w-8 fill-current text-white-white" 
                             aria-hidden="true"
                         >
                             <path d="M26.025 14.496l-14.286-.001 6.366-6.366L15.979 6 5.975 16.003 15.971 26l2.129-2.129-6.367-6.366h14.29z"/>
@@ -89,10 +90,10 @@ export default function ProfileSelector(): JSX.Element {
     }
 
     return (
-        <div className="bg-blue-darker-blue rounded-md p-2">
-            <p>Avatar</p>
-            <div className="grid grid-cols-3">
-                <div className="flex flex-col justify-center space-y-2">
+        <div className="bg-blue-darker-blue rounded-md p-4 pt-2 md:max-w-xs">
+            <Title>Avatar</Title>
+            <div className="mt-4 grid grid-cols-3 grid-flow-col auto-cols-min">
+                <div className="flex flex-col justify-between">
                     <SelectButton direction="left" name="Hat" onClick={previousHat}/>
                     <SelectButton direction="left" name="Body" onClick={previousBody}/>
                     <SelectButton direction="left" name="Lead" onClick={previousLead}/>
@@ -102,16 +103,16 @@ export default function ProfileSelector(): JSX.Element {
                     <img className="rounded-full border-2 border-yellow-dark-yellow" src={avatarUrls[currentUrlIndex]}/>
                 </div>
 
-                <div className="flex flex-col justify-center space-y-2">
+                <div className="flex flex-col justify-between">
                     <SelectButton direction="right" name="Hat" onClick={nextHat}/>
                     <SelectButton direction="right" name="Body" onClick={nextBody}/>
                     <SelectButton direction="right" name="Lead" onClick={nextLead}/>
                 </div>
             </div>
 
-            <div>
-                <p>Pseudo: {userName}</p>
-                <input type="text" onChange={handleUserName} />
+            <div className="mt-4">
+                <Title>Pseudo</Title>
+                <input className="bg-blue-200 w-full border-2 rounded border-yellow-light-yellow pl-2 text-white-white" type="text" onChange={handleUserName} />
             </div>
         </div>
     )
