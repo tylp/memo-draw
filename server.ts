a0cb7a9acd6698103c313dafe6a80e492786f08b
vbouillennec <val.bouillennec@gmail.com>
2021-07-25 19:23:24 +0200

    test

--- server.ts
+++ server.ts
@@ -4,7 +4,7 @@ const io = require('socket.io')(server);
 const next = require('next');
 
 const port = parseInt(process.env.PORT, 10) || 3000;
-const dev = process.env.NODE_ENV !== 'production';
+const dev = process.env.NODE_ENV !== 'development';
 const nextApp = next({ dev });
 const nextHandler = nextApp.getRequestHandler();
 
