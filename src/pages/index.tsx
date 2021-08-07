7b24559dd907f9379a48a6a6af4b06157e195eb5
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-08-07 18:47:22 +0200

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import {Layout, SectionTitle} from "../components/Common";
 import Loading from '../components/Common/Loading/Loading';
 import {ProfileSelector} from "../components/Home";
 import RuleItem from '../components/Home/RuleList/RuleItem/RuleItem';
-import RoomSelector from '../components/RoomSelector';
 import { useSocket } from '../hooks';
 import useLocalStorage from '../hooks/useLocalStorage';
 
@@ -11,7 +10,6 @@ export default function Index(): JSX.Element {
 	const socket = useSocket();
 	const [isLoading, setIsLoading] = useState(true);
 	const [username, setUsername] = useState<string>("");
-	const [profileIsComplete, setProfileIsComplete] = useState<boolean>(false);
 	const [profileStorage, setProfileStorage] = useLocalStorage("profile")
 
 	useEffect(() => {
@@ -33,17 +31,15 @@ export default function Index(): JSX.Element {
 		}
 		
 		socket.emit("update-profile", profile, () => {
-			setProfileIsComplete(true);
 			setProfileStorage(profile);
+			handleRoomCreation();
 		})
 	}
 
-	const handleDisconnect = () => {
-		setProfileIsComplete(false);
-	}
-
 	const handleRoomCreation = () => {
-		socket.emit('create-room');
+		socket.emit('create-room', (room) => {
+			document.location.href = `/room/${room.id}`
+		});
 	}
 
 	return (
@@ -54,32 +50,22 @@ export default function Index(): JSX.Element {
 				<Loading/>
 			) 
 			: (
-				!profileIsComplete
-				? (
-					<Layout>
-						<div className="flex flex-wrap flex-auto justify-center md:space-x-32">
-							<div>
-								<SectionTitle hintColor="text-pink-dark-pink">THE GAME</SectionTitle>
-								<RuleItem id={1} title="Invite tes copaing" content="Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet..."/>
-								<RuleItem id={2} title="Invite tes copaing" content="Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet..."/>
-								<RuleItem id={3} title="Invite tes copaing" content="Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet..."/>
-							</div>
-							<div >
-								<ProfileSelector 
-								handleStart={handleStart}
-								handleUserName={(e) => setUsername(e.currentTarget.value)}
-								username={username}/>
-							</div>
+				<Layout>
+					<div className="flex flex-wrap flex-auto justify-center md:space-x-32">
+						<div>
+							<SectionTitle hintColor="text-pink-dark-pink">THE GAME</SectionTitle>
+							<RuleItem id={1} title="Invite tes copaing" content="Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet..."/>
+							<RuleItem id={2} title="Invite tes copaing" content="Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet..."/>
+							<RuleItem id={3} title="Invite tes copaing" content="Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet... Lorem Ipsum Dolor sit amet..."/>
 						</div>
-					</Layout>
-				)
-			: (
-				<RoomSelector
-					socket={socket}
-					onHandleRoomCreation={handleRoomCreation}
-					onHandleDisconnect={handleDisconnect}
-				/>
-				)
+						<div >
+							<ProfileSelector 
+							handleStart={handleStart}
+							handleUserName={(e) => setUsername(e.currentTarget.value)}
+							username={username}/>
+						</div>
+					</div>
+				</Layout>
 			)
 			}
 		</div>
