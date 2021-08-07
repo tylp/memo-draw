316f158f6c86c7abe758213944636330e2f78fe1
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-08-07 19:12:58 +0200

    redo room/id page design

--- src/pages/room/[id].tsx
+++ src/pages/room/[id].tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useSocketRoom} from '../../hooks';
-import Link from 'next/link';
-import { Layout, Title } from '../../components/Common';
+import { Layout, SectionTitle, Title } from '../../components/Common';
 import Button from '../../components/Common/Button/Button';
 import Loading from '../../components/Common/Loading/Loading';
 import Canvas from '../../components/Canvas';
@@ -17,9 +16,9 @@ const Room = (): JSX.Element => {
 		}
 		return "";
 	}
-
+	
 	const roomId = getRoomId();
-
+	
 	const [isLoading, setIsLoading] = useState(true);
 	const [broadcastedCoords, setBroadcastedCoords] = useState(null);
 	const [sessionId] = useLocalStorage("sessionId");
@@ -29,18 +28,18 @@ const Room = (): JSX.Element => {
 	const [storageProfile] = useLocalStorage<IProfile>("profile");
 	const [message, setMessage] = useState("");
 	const [messages, setMessages] = useState([]);
-
+	
 	useEffect(() => {
 		if(!socket) return;		
 		socket.emit("join-room", roomId, (data) => {
 			if(data === false) {
 				// TODO: Redirect to homepage, because room does not exist or player is unable to join.
 			}
-
+			
 			setIsLoading(false)
 			setRoom(data);
 		})
-
+		
 		socket.on("update-room", (data) => {
 			setRoom(data);
 		})
@@ -48,7 +47,7 @@ const Room = (): JSX.Element => {
 		socket.on("receive-message-room", (message) => {
 			setMessages((prev) => [...prev, message]);
 		})
-
+		
 		socket.on("receive-drawing", (coords) => {
 			setBroadcastedCoords(coords)
 		})
@@ -57,79 +56,79 @@ const Room = (): JSX.Element => {
 	const handleMessage = (e) => {
 		setMessage(e.currentTarget.value);
 	}
-
+	
 	const sendMessage = (e) => {
 		e.preventDefault();
 		receiveMessage({
 			username: storageProfile.username,
 			content: message
 		})
-
+		
 		if(message !== '') {
 			socket.emit("send-message-room", message, roomId);
 			setMessage('');
 		}
 	}
-
+	
 	const receiveMessage = (message) => {
 		setMessages(prev => [...prev, message])
 	}
-
+	
 	// const paperClip = (e) => {
 	// 	e.preventDefault();
 	// 	navigator.clipboard.writeText(url).then(function() {
 	// 		// TODO: inform user that link is in his clipboard now		
 	// 	});
 	// }
-
+	
 	const emitCoords = (coords) => {
 		socket.emit('send-drawing', coords, roomId);
 	}
-
-	return (
-		<div>
-			{isLoading ? 
-				<Loading/>
-			: 
-			room ?
+	
+	return isLoading ? 
+		<Loading/>
+		:
+		room ? (
 			<Layout>
-					<Link href="/">
-						<a><Button>&larr; Quitter le salon</Button></a>
-					</Link>
-					<Canvas width="800" height="400"
+				<div className="flex flex-wrap flex-auto justify-center md:space-x-32">
+					<div>
+						<SectionTitle hintColor="text-yellow-light-yellow">Players</SectionTitle>
+					</div>
+					<div>
+						<Canvas width="800" height="400"
 						emitCoords={emitCoords}
 						broadcastedCoords={broadcastedCoords}
-					/>
-					<p className="text-white-white font-bold">utilisateurs: {JSON.stringify(room.players)}</p>
-					{/* <Button onClick={paperClip}>Envoyer le lien à vos amis</Button> */}
-					<Title>Messages du salon :</Title>
-					<ul>
+						/>
+						<Title>Messages du salon :</Title>
+						<ul>
 						{messages.map((msg, key) => (
-						<li className="text-white-white" key={key}>
+							<li className="text-white-white" key={key}>
 							<span className="text-white-white font-bold">{msg.username}:</span> {msg.content}
-						</li>
-						))}
-					</ul>
-					<form action="" style={{display: 'flex'}}>
-						<input 
-						type="text" 
-						onChange={handleMessage} 
-						value={message}
-						className="bg-blue-200 w-full border-2 rounded border-yellow-light-yellow pl-2 text-white-white"/>
-						<Button onClick={sendMessage}>
+							</li>
+							))}
+							</ul>
+							<form action="" style={{display: 'flex'}}>
+							<input 
+							type="text" 
+							onChange={handleMessage} 
+							value={message}
+							className="bg-blue-200 w-full border-2 rounded border-yellow-light-yellow pl-2 text-white-white"/>
+							<Button onClick={sendMessage}>
 							Envoyer
-						</Button>
-					</form>
-					<p>&nbsp;</p>
-				</Layout>
-			:
-				<Layout>
-					<Title>Ce salon n`&apos;`existe pas, veuillez reessayer: </Title>
-					<Button><a href="/">Créer un salon / rejoindre un salon</a></Button>
-				</Layout> 
-			}
-		</div>
-	)
-}
-
+							</Button>
+							</form>
+						</div>
+						<div>
+							<SectionTitle hintColor="text-pink-dark-pink">Game</SectionTitle>
+						</div>
+				</div>
+			</Layout>
+		)
+		: (
+			<Layout>
+				<Title>Ce salon n`&apos;`existe pas, veuillez reessayer: </Title>
+				<Button><a href="/">Créer un salon / rejoindre un salon</a></Button>
+			</Layout> 
+		)
+}			
 export default Room;
