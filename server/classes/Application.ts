4c2c85d681f0ae30af63b6e1499721f92c162c93
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-08-07 16:57:37 +0200

    remove unused import

--- server/classes/Application.ts
+++ server/classes/Application.ts
@@ -3,7 +3,6 @@ import ISession from '../interfaces/ISession';
 import IdGeneratorService from "../services/IdGeneratorService";
 import SocketIdentifierService from '../services/SocketIdentifierService';
 import Room from "./Room";
-import RoomSocketBinder from './sockets/RoomSocketBinder';
 import SocketIoBinder from './sockets/SocketIoHandler';
 import SocketIoHandler from './sockets/SocketIoHandler';
 import RoomStorage from './Storage/RoomStorage';
