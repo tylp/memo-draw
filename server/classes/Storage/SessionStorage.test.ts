b3eadfebe1dd58d4207b794baefad686050539ad
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-08-09 13:58:41 +0200

    initiated SessionStorage

---
+++ server/classes/Storage/SessionStorage.test.ts
@@ -0,0 +1,17 @@
+import SessionStorage from "./SessionStorage";
+
+describe("SessionStorage", () => {
+	let storage = new SessionStorage();
+	
+    beforeEach(() => {
+        storage = new SessionStorage();
+    })
+
+	test("generate should work", () => {
+        //
+	});
+
+	test("update should work", () => {
+        //
+	});
+});
