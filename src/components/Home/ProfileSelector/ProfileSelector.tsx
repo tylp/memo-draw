e9fdb2bc8b24ef5ab20040478e154a43cd6c8bf6
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-08-10 00:02:45 +0200

    remove useless spaces

--- src/components/Home/ProfileSelector/ProfileSelector.tsx
+++ src/components/Home/ProfileSelector/ProfileSelector.tsx
@@ -11,22 +11,22 @@ export function SelectButton(specs: SelectButtonSpec) : JSX.Element {
     return (
         <div className="flex flex-col items-center">
             <p className="text-md text-white-white">{specs.name}</p>
-            <button 
-                onClick={specs.onClick} 
+            <button
+                onClick={specs.onClick}
                 className="bg-blue-200 hover:bg-yellow-dark-yellow text-gray-800 font-bold py-1 px-1 rounded-full inline-flex items-center transition duration-300"
             >
                 {
-                    specs.direction === 'left' 
-                    ? 
-                        <svg viewBox="0 0 32 32" 
-                            className="icon icon-arrow-left w-8 fill-current text-white-white" 
+                    specs.direction === 'left'
+                    ?
+                        <svg viewBox="0 0 32 32"
+                            className="icon icon-arrow-left w-8 fill-current text-white-white"
                             aria-hidden="true"
                         >
                             <path d="M26.025 14.496l-14.286-.001 6.366-6.366L15.979 6 5.975 16.003 15.971 26l2.129-2.129-6.367-6.366h14.29z"/>
                         </svg>
-                    : 
-                        <svg viewBox="0 0 32 32" 
-                            className="transform rotate-180 icon icon-arrow-left w-8 fill-current text-white-white" 
+                    :
+                        <svg viewBox="0 0 32 32"
+                            className="transform rotate-180 icon icon-arrow-left w-8 fill-current text-white-white"
                             aria-hidden="true"
                         >
                             <path d="M26.025 14.496l-14.286-.001 6.366-6.366L15.979 6 5.975 16.003 15.971 26l2.129-2.129-6.367-6.366h14.29z"/>
