27d1efb7a46f02ec55b16b42cb19099f351b0dc1
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-08-09 20:26:01 +0200

    add tests for session storage

--- server/classes/Storage/SessionStorage.test.ts
+++ server/classes/Storage/SessionStorage.test.ts
@@ -1,3 +1,5 @@
+import { FaceType, RubberColor } from './../../interfaces/IProfile';
+import { BodyColor } from "../../interfaces/IProfile";
 import SessionStorage from "./SessionStorage";
 
 describe("SessionStorage", () => {
@@ -8,10 +10,32 @@ describe("SessionStorage", () => {
     })
 
 	test("generate should work", () => {
-        //
+        expect(storage.isEmpty()).toBeTruthy()
+        const session = storage.generate();
+        expect(session).toHaveProperty("sessionId");
+        expect(session).toHaveProperty("playerId");
+        expect(session).toHaveProperty("profile");
+        expect(session.profile).toHaveProperty("username");
+        expect(session.profile).toHaveProperty("rubberColor");
+        expect(session.profile).toHaveProperty("bodyColor");
+        expect(session.profile).toHaveProperty("faceType");
 	});
 
 	test("update should work", () => {
-        //
+        let session = storage.generate();
+        expect(session).toBe(storage.get(session.sessionId));
+        const newUsername = "New username";
+
+        storage.update(session.sessionId, {
+            profile: {
+            username: newUsername,
+            bodyColor: BodyColor.Blue,
+            faceType: FaceType.Astonished,
+            rubberColor: RubberColor.Blue,
+        }});
+        
+        session = storage.get(session.sessionId);
+
+        expect(session.profile.username).toBe(newUsername);
 	});
 });
