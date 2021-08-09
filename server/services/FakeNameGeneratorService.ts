79d7cedd5090aa593409863c0b2ea55be3ebec2d
Adrien N. F <iamnautilusjungle@gmail.com>
2021-08-09 11:26:35 +0200

    Update FakeNameGeneratorService.ts

--- server/services/FakeNameGeneratorService.ts
+++ server/services/FakeNameGeneratorService.ts
@@ -1,8 +1,10 @@
 export default class FakeNameGeneratorService {
     static NAMES = [
-        "Charles",
-        "Xavier",
-        "Nicolas"
+        "Adrien",
+        "Antoine",
+        "Cyril",
+        "Nathan",
+        "Valentin"
     ];
 
     static generate(): string {
