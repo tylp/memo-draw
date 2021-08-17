a43fd50244ce06dbf095c2b1309c37ef9d3ddba4
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-08-17 18:53:09 +0200

    update return type for AvatarService

--- src/services/AvatarService.ts
+++ src/services/AvatarService.ts
@@ -1,11 +1,11 @@
 import { FaceType, BodyType } from "../../server/interfaces/IProfile";
 
 export default class AvatarService {
-    public static getBody(bodyType: BodyType): NodeRequire {
+    public static getBody(bodyType: BodyType): string {
         return require(`../avatars/body/${bodyType}`);
     }
 
-    public static getFaceType(faceType: FaceType): NodeRequire {
+    public static getFaceType(faceType: FaceType): string {
         return require(`../avatars/expression/${faceType}`);
     }
 }
