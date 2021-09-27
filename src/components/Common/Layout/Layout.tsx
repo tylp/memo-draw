6012112b6bfcf2784ca0835be1d9bc78fae9ada0
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-09-27 21:34:24 +0200

    sorry eslint #86

--- src/components/Common/Layout/Layout.tsx
+++ src/components/Common/Layout/Layout.tsx
@@ -1,5 +1,5 @@
-import React, { ReactNode } from "react";
-import Logo from "../Logo/Logo";
+import React, { ReactNode } from 'react';
+import Logo from '../Logo/Logo';
 
 interface LayoutSpec {
     children: ReactNode
@@ -7,27 +7,27 @@ interface LayoutSpec {
 
 export default function Layout({ children }: LayoutSpec) : JSX.Element {
     return(
-        <div className="
+        <div className='
             w-screen
             h-screen
             bg-gradient-to-r
             from-blue-blue to-blue-light-blue
-        ">
-            <div className="
+        '>
+            <div className='
                 flex
                 flex-col
                 justify-center
                 items-center
-            ">
+            '>
                 <Logo/>
-                <div className="
+                <div className='
                     px-32
                     py-16
                     border-8
                     border-white-white
                     border-opacity-10
                     rounded-xl
-                ">
+                '>
                     {children}
                 </div>
             </div>
