cf4750dd4c478e4ec11b216b2a89180bae3604b4
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-09-27 21:36:10 +0200

    spaces to tab

--- src/components/Common/Layout/Layout.tsx
+++ src/components/Common/Layout/Layout.tsx
@@ -6,32 +6,32 @@ interface LayoutSpec {
 }
 
 export default function Layout({ children }: LayoutSpec) : JSX.Element {
-    return(
-        <div className='
-            w-screen
-            h-screen
-            bg-gradient-to-r
-            from-blue-blue to-blue-light-blue
-        '>
-            <div className='
-                flex
-                flex-col
-                justify-center
-                items-center
-            '>
-                <Logo/>
-                <div className='
-                    px-32
-                    py-16
-                    border-8
-                    border-white-white
-                    border-opacity-10
-                    rounded-xl
-                '>
-                    {children}
-                </div>
-            </div>
-        </div>
-        
-    )
+	return(
+		<div className='
+			w-screen
+			h-screen
+			bg-gradient-to-r
+			from-blue-blue to-blue-light-blue
+		'>
+			<div className='
+				flex
+				flex-col
+				justify-center
+				items-center
+			'>
+				<Logo/>
+				<div className='
+					px-32
+					py-16
+					border-8
+					border-white-white
+					border-opacity-10
+					rounded-xl
+				'>
+					{children}
+				</div>
+			</div>
+		</div>
+		
+	)
 }
