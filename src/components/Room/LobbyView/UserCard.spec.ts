259de987fe293fb98fbf171d03ef396538c9e246
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-10-12 12:18:55 +0200

    host can't kick itself now

--- src/components/Room/LobbyView/UserCard.spec.ts
+++ src/components/Room/LobbyView/UserCard.spec.ts
@@ -3,7 +3,6 @@ import Player from '../../../../server/classes/Player';
 export interface UserCardSpec {
 	player: Player;
 	creatorId?: string;
-	currentPlayer?: Player;
+	currentPlayerId?: string | unknown;
 	subtitle?: string;
-	isCreator: boolean;
 }
