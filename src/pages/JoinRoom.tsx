5608c4ca0e8027097b717f1964ca765082cd2e97
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-10-12 15:13:40 +0200

    fix linter

--- src/pages/JoinRoom.tsx
+++ src/pages/JoinRoom.tsx
@@ -2,8 +2,6 @@ import React, { useEffect } from 'react';
 import { useSocketRoom } from '../hooks';
 import Loading from '../components/Common/Loading/Loading';
 import { useParams, useHistory } from 'react-router-dom';
-import useLocalStorage from '../hooks/useLocalStorage/useLocalStorage';
-import { LocalStorageKey } from '../hooks/useLocalStorage/useLocalStorage.types';
 
 export default function JoinRoom(): JSX.Element {
 	const { roomId } = useParams();
