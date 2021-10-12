78b753b6a2efb2dc60d668e90f7d37f7e821d715
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-10-12 12:33:34 +0200

    fixed countdown not going down and renamed old props

--- src/components/Room/GameView/Countdown/Countdown.tsx
+++ src/components/Room/GameView/Countdown/Countdown.tsx
@@ -1,24 +1,23 @@
 import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 
-interface TimerProps {
+interface CountdownProps {
 	limitDate: moment.Moment;
 	onFinish: () => void;
 }
 
-export default function Timer(props: TimerProps): JSX.Element {
+export default function Countdown(props: CountdownProps): JSX.Element {
 
 	const [timeLeft, setTimeLeft] = useState<number>();
 	const [currentInterval, setCurrentInterval] = useState<NodeJS.Timer>();
 
-	const timeBetweenNowAndLimitDate = moment(props.limitDate).diff(moment(), 'milliseconds');
-
 	const updateTimeLeft = () => {
+		const timeBetweenNowAndLimitDate = moment(props.limitDate).diff(moment(), 'milliseconds');
 		setTimeLeft(Math.max(0, timeBetweenNowAndLimitDate));
 	}
 
 	const clearIntervalIfExist = () => {
-		if(currentInterval) clearInterval(currentInterval);
+		if (currentInterval) clearInterval(currentInterval);
 	}
 
 	useEffect(() => {
@@ -26,28 +25,28 @@ export default function Timer(props: TimerProps): JSX.Element {
 
 		updateTimeLeft();
 
-		if(!props.limitDate) return;
+		if (!props.limitDate) return;
 
 		setCurrentInterval(setInterval(() => {
 			updateTimeLeft()
 		}, 10));
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [props.limitDate])
 
-	useEffect(() => {			
-		if(moment().isAfter(props.limitDate)) {
+	useEffect(() => {
+		if (moment().isAfter(props.limitDate)) {
 			clearIntervalIfExist();
 
-			if(props.onFinish) {
+			if (props.onFinish) {
 				props.onFinish();
 			}
 		}
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [timeLeft, currentInterval])
 
 	return props.limitDate ? (
 		<div className="max-w-sm bg-blue-darker-blue rounded-md p-3 h-16 w-16">
-			<div className="text-lg font-semibold text-white-white">{Math.floor(timeLeft/1000)} s</div>
+			<div className="text-lg font-semibold text-white-white">{Math.floor(timeLeft / 1000)} s</div>
 		</div>
 	) : <div>Vide</div>
 }
