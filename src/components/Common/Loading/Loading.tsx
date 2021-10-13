1d16d22b5aa4913e071e308dabce83e21728debe
tylp <azertyzhd@gmail.com>
2021-10-13 12:34:18 +0200

    Implement requested changes

--- src/components/Common/Loading/Loading.tsx
+++ src/components/Common/Loading/Loading.tsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import { BodyColor, BodyType, FaceType, RubberColor } from '../../../../server/interfaces/IProfile';
+import AvatarFactory from '../../../../server/factories/AvatarFactory';
 import Avatar from '../Avatar/Avatar';
 import { IAvatar } from '../Avatar/Avatar.spec';
 import Title from '../Title/Title';
 
 export default function Loading(): JSX.Element {
 
-	const a: IAvatar = randomAvatar();
-
-	function randomAvatar(): IAvatar {
-
-		return {
-			rubberColor: RubberColor[Object.keys(RubberColor)[Math.floor(Math.random() * Object.keys(RubberColor).length)]],
-			bodyType: BodyType[Object.keys(BodyType)[Math.floor(Math.random() * Object.keys(BodyType).length)]],
-			bodyColor: BodyColor[Object.keys(BodyColor)[Math.floor(Math.random() * Object.keys(BodyColor).length)]],
-			faceType: FaceType[Object.keys(FaceType)[Math.floor(Math.random() * Object.keys(FaceType).length)]]
-		};
-	}
-
+	const a: IAvatar = AvatarFactory.create();
 	return (
 		<div>
 			<Title>Loading...</Title>
-			<br/>
 			<div className="animate-spin-bezier">
 				<Avatar avatar={a}/>		
 			</div>
