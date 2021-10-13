532bfef1017d618f5c190d7f626130475bf64286
tylp <azertyzhd@gmail.com>
2021-10-13 13:37:58 +0200

    remove title and unecessary containers

--- src/components/Common/Loading/Loading.tsx
+++ src/components/Common/Loading/Loading.tsx
@@ -2,17 +2,13 @@ import React from 'react';
 import AvatarFactory from '../../../../server/factories/AvatarFactory';
 import Avatar from '../Avatar/Avatar';
 import { IAvatar } from '../Avatar/Avatar.spec';
-import Title from '../Title/Title';
 
 export default function Loading(): JSX.Element {
 
 	const a: IAvatar = AvatarFactory.create();
 	return (
-		<div>
-			<Title>Loading...</Title>
-			<div className="animate-spin-bezier">
-				<Avatar avatar={a}/>		
-			</div>
+		<div className="animate-spin-bezier h-28 w-28">
+			<Avatar avatar={a} />
 		</div>
 	)
 }
