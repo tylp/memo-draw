12c50458c1873062d605af929c5da4e9947a4d8e
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-10-13 22:49:53 +0200

    add desgressive countdown

--- server/classes/Game.ts
+++ server/classes/Game.ts
@@ -2,23 +2,23 @@ import Player from './Player';
 import Room from './Room';
 import { shuffle } from 'lodash';
 import dayjs, { Dayjs } from 'dayjs';
+import { number } from 'prop-types';
 
 export class Game {
 	id: string;
 	creatorPlayerId: string;
 	players: Array<Player>;
-	currentDrawingIndex: number;
-	currentNumberOfDrawings: number;
-	currentPlayerIndex: number;
+	currentDrawingIndex: number = 0;
+	currentNumberOfDrawings: number = 0;
+	currentPlayerIndex: number = 0;
 	limitDate: Dayjs;
+	minSeconds: number = 4;
+	maxSeconds: number = 10;
 
 	constructor(room: Room) {
 		this.id = room.id;
 		this.creatorPlayerId = room.creatorPlayerId;
 		this.players = shuffle(room.players);
-		this.currentDrawingIndex = 0;
-		this.currentNumberOfDrawings = 0;
-		this.currentPlayerIndex = 0;
 		this.refreshLimitDate();
 	}
 
@@ -46,6 +46,14 @@ export class Game {
 	}
 
 	refreshLimitDate(): void {
-		this.limitDate = dayjs().add(5, 'seconds');
+		this.limitDate = dayjs().add(this.getSecondsToDraw(), 'seconds');
+	}
+
+	getSecondsToDraw(): number {
+		if (this.currentDrawingIndex === 0)
+			return this.maxSeconds;
+		if (this.currentDrawingIndex > 20)
+			return this.minSeconds;
+		return -2 * Math.log(this.currentDrawingIndex) + 10;
 	}
 }
