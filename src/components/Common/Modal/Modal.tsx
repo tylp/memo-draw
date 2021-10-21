5cb4a543ccffdac55dd9d06916fcb83a2d2fc2f0
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-10-22 00:00:30 +0200

    update modal

--- src/components/Common/Modal/Modal.tsx
+++ src/components/Common/Modal/Modal.tsx
@@ -5,9 +5,9 @@ import { useTranslation } from 'react-i18next';
 interface ModalProps {
 	visible: boolean;
 	children: ReactNode,
-	onClose?: (() => void) | undefined;
+	onClose: (() => void);
 	closeButtonTranslationKey?: string | undefined;
-	onValidate?: (() => void) | undefined;
+	onValidate?: (() => void);
 	validateButtonTranslationKey?: string | undefined;
 }
 
@@ -27,7 +27,7 @@ export default function Modal(props: ModalProps): JSX.Element {
 				<div className="relative w-auto my-6 mx-auto max-w-3xl">
 					<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white-white outline-none focus:outline-none">
 						<div className="flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t">
-							<SectionTitle>bg-blue-darker-blue</SectionTitle>
+							<SectionTitle>{t('lobbyView.editProfileBtnLabel')}</SectionTitle>
 						</div>
 						<div className="relative p-6 flex-auto">
 							{props.children}
@@ -36,14 +36,14 @@ export default function Modal(props: ModalProps): JSX.Element {
 							<button
 								className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 								type="button"
-								onClick={() => props.onClose && props.onClose()}
+								onClick={() => props.onClose()}
 							>
 								{t(props.closeButtonTranslationKey)}
 							</button>
 							<button
 								className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
 								type="button"
-								onClick={() => props.onValidate && props.onValidate()}
+								onClick={() => props.onValidate()}
 							>
 								{t(props.validateButtonTranslationKey)}
 							</button>
