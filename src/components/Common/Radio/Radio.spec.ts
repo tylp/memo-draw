6261a1f806866aec331edfd0960b5b9a58a96818
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-10-21 18:53:03 +0200

    spaces to tabs + delete radio.spec

--- src/components/Common/Radio/Radio.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from 'react';
-
-export interface RadioSpec {
-    className?: string,
-    size: 'small' | 'medium' | 'big',
-    color: 'primary' | 'secondary' | 'light-secondary',
-}
