c18d30e87574c0639f12154ca501d49950e8acac
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-10-21 18:38:05 +0200

    merge main

--- src/components/Common/Radio/Radio.tsx
+++ src/components/Common/Radio/Radio.tsx
@@ -3,26 +3,20 @@ import { RadioSpec } from "./Radio.spec";
 
 export default function Radio(props: RadioSpec): JSX.Element {
 
-    const [className, setClassName] = useState('');
+    const [inputClassName, setInputClassName] = useState('');
+    const [labelClassName, setLabelClassName] = useState('');
 
     useEffect(() => {
         let color = '';
 
-		if (props.color === 'primary') {
-			color = 'bg-pink-dark-pink hover:bg-pink-light-pink text-white-white';
-		}
-		else if (props.color === 'secondary') {
-			color = 'bg-blue-darker-blue hover:bg-blue-blue text-yellow-light-yellow';
-		}
-		else if (props.color === 'light-secondary') {
-			color = 'bg-grey-light-grey hover:bg-blue-blue text-yellow-light-yellow';
+		if (props.color === 'light-secondary') {
+			color = 'bg-grey-light-grey hover:bg-blue-blue text-yellow-light-yellow focus:bg-pink-light-pink';
 		}
 
-        setClassName(`
+        setInputClassName(`
             rounded-md
+            opacity-0 w-0 fixed
             ${props.className}
-            ${props.size === 'small' ? 'pl-4 pr-3 m-1 mr-1 h-8' : null}
-            ${props.size === 'medium' ? 'pl-4 pr-4 pt-1 ml-1 mr-1 pb-1' : null}
             ${props.size === 'big' ? 'w-1/2 pt-4 pb-4 ml-1 mr-1' : null}
             transition duration-300
             font-rubik-bold uppercase
@@ -33,19 +27,19 @@ export default function Radio(props: RadioSpec): JSX.Element {
 
     return (
         <>
-            <div className={className}>
-                <input type="radio" className="hidden" id="slow" name="drone" value="huey" checked />
-                <label htmlFor="slow">Slow</label>
+            <div >
+                <input type="radio" className={inputClassName} name="speed" value="slow" checked />
+                <label htmlFor="slow" className={labelClassName}>Slow</label>
             </div>
 
-            <div className={className}>
-                <input type="radio" id="normal" name="drone" value="dewey" />
-                <label htmlFor="normal">Normal</label>
+            <div>
+                <input type="radio" className={inputClassName} name="speed" value="normal" />
+                <label htmlFor="normal" className={labelClassName}>Normal</label>
             </div>
 
-            <div className={className}>
-                <input type="radio" id="fast" name="drone" value="louie" />
-                <label htmlFor="fast">Fast</label>
+            <div>
+                <input type="radio" className={inputClassName} name="speed" value="fast" />
+                <label htmlFor="fast" className={labelClassName}>Fast</label>
             </div>
         </>
     )
--- src/components/Lobby/LobbyView/GameSetting/GameSetting.tsx
+++ src/components/Lobby/LobbyView/GameSetting/GameSetting.tsx
@@ -8,14 +8,14 @@ export function GameSetting(): JSX.Element {
         <div className="pt-2 pb-2 pl-2 pr-2 h-auto w-96 bg-blue-darker-blue rounded-md flex flex-col">
             <div className="flex flex-row justify-between">
                 <Title>Speed</Title>
-                <FontAwesomeIcon icon={faStopwatch} />
+                <FontAwesomeIcon className="text-yellow-light-yellow" icon={faStopwatch} />
             </div>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </p>
+            <p className="text-white-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam </p>
             <div className="flex flex-row justify-around">
-                <Radio color='light-secondary' size='small'/>
+                <Radio color='light-secondary' size='big'/>
             </div>
             <div className="flex flex-row justify-around">
-            <Button size="big" color="light-secondary">Slow</Button>
+                <Button size="big" color="light-secondary">Slow</Button>
                 <Button size="big" color="light-secondary">Normal</Button>
                 <Button size="big" color="light-secondary">Fast</Button>
             </div>
