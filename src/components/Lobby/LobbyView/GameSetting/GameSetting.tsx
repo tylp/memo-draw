30d2b8459cfa662810c5a30d9d66f33d01c9a904
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-10-22 18:00:43 +0200

    fix radiolist margin

--- src/components/Lobby/LobbyView/GameSetting/GameSetting.tsx
+++ src/components/Lobby/LobbyView/GameSetting/GameSetting.tsx
@@ -18,14 +18,14 @@ export function GameSetting(props: GameSettingSpec): JSX.Element {
 	const gameSettingPropertyValues = Object.values(props.list)
 
 	return (
-		<div className="pt-3 pb-3 pl-3 pr-3 mr-4 h-auto w-96 bg-blue-darker-blue rounded-md flex flex-col">
-			<div className="flex flex-row justify-between">
-				<Title>{t(`${props.for}.title`)}</Title>
-				<FontAwesomeIcon className="text-yellow-light-yellow text-2xl" icon={faStopwatch} />
+		<div className="pt-3 pb-3 pl-3 pr-3 mr-4 h-auto w-96 bg-blue-darker-blue rounded-md flex flex-col self-stretch justify-between">
+			<div>
+				<div className="flex flex-row justify-between">
+					<Title>{t(`${props.for}.title`)}</Title>
+					<FontAwesomeIcon className="text-yellow-light-yellow text-2xl" icon={faStopwatch} />
+				</div>
+				<p className="leading-tight text-white-white">{t(`${props.for}.description`)}</p>
 			</div>
-
-			<p className="leading-tight text-white-white">{t(`${props.for}.description`)}</p>
-
 			<div className="flex flex-row justify-around">
 				<RadioList list={gameSettingPropertyValues} currentValue={props.currentValue} setCurrentValue={props.setCurrentValue} color='light-secondary' size='large' />
 			</div>
