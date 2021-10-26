ab645837d36c1b3d8d9ce128721799ac0d8f244e
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-10-26 15:13:58 +0200

    remove magic number #203

--- server/services/ProfileValidatorService.ts
+++ server/services/ProfileValidatorService.ts
@@ -1,7 +1,9 @@
 import IProfile from '../interfaces/IProfile';
 
+const MIN_LENGTH_USERNAME = 3;
+
 export default class ProfileValidatorService {
 	static isProfileValid(profile: IProfile): boolean {
-		return profile.username.length >= 3;
+		return profile.username.length >= MIN_LENGTH_USERNAME;
 	} 
 }
