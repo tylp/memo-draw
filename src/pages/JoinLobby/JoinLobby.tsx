ad9da1210db647620cfc80f80c286a6e95f2d51c
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-11-11 08:45:07 +0100

    remove useless import

--- src/pages/JoinLobby/JoinLobby.tsx
+++ src/pages/JoinLobby/JoinLobby.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSocketLobby } from '../../hooks';
 import { useParams, useHistory } from 'react-router-dom';
 import { useDangerSnackbar } from '../../hooks/useSnackbar/useSnackbar'
