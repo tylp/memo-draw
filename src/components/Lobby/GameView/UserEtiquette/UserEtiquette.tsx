57f8f01728674ec84b77d6709404e2ba00d7895d
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-11-18 19:36:20 +0100

    Rename userProfile -> userEtiquette

--- src/components/Lobby/GameView/UserEtiquette/UserEtiquette.tsx
+++ src/components/Lobby/GameView/UserEtiquette/UserEtiquette.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import Player from '../../../../../server/classes/Player';
+import { Avatar } from '../../../Common';
 
-interface UserProfileSpec {
+interface UserEtiquetteSpec {
 	player: Player;
 	creatorId: string;
 	currentPlayer: Player;
 }
 
-export default function UserProfile(props: UserProfileSpec): JSX.Element {
+export default function UserProfile(props: UserEtiquetteSpec): JSX.Element {
 	return (
 		<p className="bg-blue-200 w-full border-2 rounded border-yellow-light-yellow pl-2 text-white-white">
+			<Avatar avatar={props.player.profile.avatar} />
 			{props.player.profile.username} {props.currentPlayer.id === props.player.id ? 'Current' : ''}
 		</p>
 	)
