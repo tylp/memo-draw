3ee5ec5d2f94920800fafb32b177b0ac83032a44
Nathan Gobinet <nathan.gobinet@gmail.com>
2021-11-17 22:38:12 -0500

    Fix: force engine bounds update on component painted

--- src/components/Lobby/GameView/Canvas/Canvas.tsx
+++ src/components/Lobby/GameView/Canvas/Canvas.tsx
@@ -1,6 +1,16 @@
 import React, { CSSProperties, useEffect, useRef, useState } from 'react';
 import { Engine, drawState, ShapeType } from 'memo-draw-engine';
 
+// Util function used to trigger when a component is fully painted
+// Sources:
+// https://stackoverflow.com/a/34999925
+// https://github.com/facebook/react/issues/20863
+function onPainted(callback) {
+	window.requestAnimationFrame(() => {
+		setTimeout(() => { callback(); }, 0);
+	});
+}
+
 export default function Canvas(): JSX.Element {
 	const [backgroundStyle, setBackgroundStyle] = useState<CSSProperties>();
 	const canvasRef = useRef<HTMLCanvasElement>();
@@ -11,6 +21,10 @@ export default function Canvas(): JSX.Element {
 		drawState.shapeType = ShapeType.Pencil;
 		drawState.thickness = 5;
 		initBackgroundStyle();
+
+		// Manually update bounds when component is painted
+		// Needed to correctly handle mouse position on canvas
+		onPainted(() => { engine.canvasManager.updateBounds(); });
 	}, []);
 
 	const initBackgroundStyle = (): void => {
