de1814a77ddfc93112b21e916a057e85cf307433
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-11-22 12:29:22 +0100

    fix userEtiquette & remove width props to sectionTitle

--- src/components/Lobby/GameView/UserEtiquette/UserEtiquette.tsx
+++ src/components/Lobby/GameView/UserEtiquette/UserEtiquette.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Player from '../../../../../server/classes/Player';
 import { Avatar } from '../../../Common';
+import { useTranslation } from 'react-i18next';
 
 interface UserEtiquetteSpec {
 	player: Player;
@@ -9,9 +10,11 @@ interface UserEtiquetteSpec {
 }
 
 export default function UserEtiquette(props: UserEtiquetteSpec): JSX.Element {
+	const { t } = useTranslation();
+
 	return (
 		<div
-			className="relative mb-2 flex flex-row items-center bg-blue-darker-blue w-full h-20 pl-2 text-white-white"
+			className="relative mb-2 flex flex-row items-center bg-blue-darker-blue w-full h-16 pl-2 text-white-white"
 			style={{
 				borderTopLeftRadius: '3em',
 				borderBottomLeftRadius: '3em',
@@ -19,15 +22,13 @@ export default function UserEtiquette(props: UserEtiquetteSpec): JSX.Element {
 				borderBottomRightRadius: '0.5em',
 			}}
 		>
-			<div className="w-14 ml-1">
+			<div className="flex-shrink-0 w-10 ml-1">
 				<Avatar avatar={props.player.profile.avatar} />
 			</div>
-			<div className="text-lg ml-4 font-semibold text-white-white">
+			<div className="flex-1 text-lg m-4 font-semibold text-white-white truncate">
 				{props.player.profile.username}
 			</div>
-			<div className="absolute -right-8 bottom-0 pl-1 pr-1 m-0 h-5 rounded-lg transform -rotate-12 bg-pink-dark-pink text-sm font-rubik-bold text-white-white">
-				{props.currentPlayer.id === props.player.id ? 'Drawing' : ''}
-			</div>
+			{props.currentPlayer.id === props.player.id && (<div className="absolute -right-8 bottom-0 pl-1 pr-1 m-0 h-5 rounded-lg transform -rotate-12 bg-pink-dark-pink text-sm font-rubik-bold text-white-white">{t('gameView.currentlyDrawing')}</div>)}
 		</div>
 	)
 }
