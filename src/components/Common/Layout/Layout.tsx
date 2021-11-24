a1a0bd47a25b1f50acf0d678a2d37198e6f9724e
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-11-24 16:04:07 +0100

    add logo in border

--- src/components/Common/Layout/Layout.tsx
+++ src/components/Common/Layout/Layout.tsx
@@ -23,8 +23,22 @@ function getCurrentWindowDimensions(): {width: number; height: number} {
 
 function WhiteBorder(props: WhiteBorderProps): JSX.Element {
 	return (
-		<div className='w-full border-8 border-white-white border-opacity-10 rounded-xl'>
-			{props.children}
+		<div className='w-full border-l-8 border-b-8 border-r-8 border-white-white border-opacity-10 rounded-b-xl'>
+			<div className='absolute border-t-8 border-white-white border-opacity-10'
+				style={{
+					left: '0.48em',
+					width: '30em',
+				}}>
+			</div>
+			<div className='absolute border-t-8 border-white-white border-opacity-10'
+				style={{
+					right: '0.48em',
+					width: '30em',
+				}}>
+			</div>
+			<div className="relative -top-16">
+				{props.children}
+			</div>
 		</div>
 	)
 }
@@ -35,15 +49,15 @@ interface LayoutSpec {
 
 
 export default function Layout({ children }: LayoutSpec): JSX.Element {
-	const MAX_RATIO = 1;
+	const MAX_RATIO = 0.95;
 
 	const firstWindowDimensions = getCurrentWindowDimensions();
-	const [dimensionRatio, setDimensionRatio] = useState(1);
+	const [dimensionRatio, setDimensionRatio] = useState(MAX_RATIO);
 
 	const debounceResizeWithRatio = useDebounce((nextRatio) => setDimensionRatio(nextRatio), 100);
 
 	function handleResize() {
-		let nextRatio = getCurrentWindowDimensions().width/firstWindowDimensions.width;
+		let nextRatio = getCurrentWindowDimensions().width / firstWindowDimensions.width;
 		if (nextRatio > MAX_RATIO)
 			nextRatio = MAX_RATIO
 		debounceResizeWithRatio(nextRatio)
@@ -55,31 +69,33 @@ export default function Layout({ children }: LayoutSpec): JSX.Element {
 	}, []);
 
 	return (
-		<div className="flex flex-col items-center w-screen h-screen min-h-screen min-w-full">
-			<div className="sm:hidden">
-				yes
+		<>
+			<div className="sm:hidden flex flex-col justify-center items-center font-rubik-bold text-lg text-white-white">
+				<p>Working on a mobile version</p>
 			</div>
-			<div className=""
-				style={{
-					transform: `scale(${dimensionRatio})`,
-				}}>
-				<Box py={2} className="flex flex-col justify-center items-center">
-					<Logo size='small' />
-				</Box>
+			<div className="flex flex-col justify-center items-center w-screen h-screen min-h-screen min-w-full">
+				<div className=""
+					style={{
+						transform: `scale(${dimensionRatio})`,
+					}}>
 
-				<Box py={4}>
-					<div className='flex flex-col'
-						style={{
-							width: '70rem',
-						}}>
-						<WhiteBorder>
-							<Box p={2}>
-								{children}
-							</Box>
-						</WhiteBorder>
-					</div>
-				</Box>
+					<Box py={4}>
+						<div className='flex flex-col'
+							style={{
+								width: '80rem',
+							}}>
+							<WhiteBorder>
+								<Box py={2} className="relative flex flex-col justify-center items-center">
+									<Logo size='medium' />
+								</Box>
+								<Box p={2}>
+									{children}
+								</Box>
+							</WhiteBorder>
+						</div>
+					</Box>
+				</div>
 			</div>
-		</div>
+		</>
 	)
 }
