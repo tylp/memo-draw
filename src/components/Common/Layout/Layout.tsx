22750d1881022ca728c48b7f27e532ccd888beee
Nathan Gobinet <nathan.gobinet@gmail.com>
2021-11-26 00:15:50 -0500

    Rework layout

--- src/components/Common/Layout/Layout.tsx
+++ src/components/Common/Layout/Layout.tsx
@@ -1,52 +1,28 @@
 import React, { ReactNode } from 'react';
-import Logo from '../Logo/Logo';
-import { Container, Row, Col } from 'react-grid-system';
-import Box from '../Box/Box';
 
-interface WhiteBorderProps {
-	children: ReactNode;
-}
-
-function WhiteBorder(props: WhiteBorderProps): JSX.Element {
-	return (
-		<div className='w-full border-8 border-white-white border-opacity-10 rounded-xl'>
-			{props.children}
-		</div>
-	)
-}
+import styles from '../../../../styles/Layout.module.css';
 
 interface LayoutSpec {
 	children: ReactNode
 }
 
-
 export default function Layout({ children }: LayoutSpec): JSX.Element {
 	return (
-		<Container fluid className="bg-gradient-to-r from-blue-blue to-blue-light-blue min-h-screen h-100 min-w-full">
-			<Row>
-				<Col sm={12} xl={10} offset={{ sm: 0, xl: 1 }}>
-					<Row>
-						<Col>
-							<Box py={2} className="flex flex-col justify-center items-center">
-								<Logo size='small' />
-							</Box>
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<div className='w-full flex flex-col justify-center items-center'>
-								<Box py={4} className="w-full">
-									<WhiteBorder>
-										<Box p={2}>
-											{children}
-										</Box>
-									</WhiteBorder>
-								</Box>
-							</div>
-						</Col>
-					</Row>
-				</Col>
-			</Row>
-		</Container>
+		<div className="bg-gradient-to-r from-blue-blue to-blue-light-blue min-h-screen">
+			<div className="flex items-center justify-center min-h-screen">
+				<div className={styles['container']}>
+					<div style={{ position: 'relative' }}>
+						<img
+							className={styles['logo']}
+							src="/img/logo.svg"
+							alt="Memo Draw"
+						/>
+					</div>
+					<main className={styles['main']}>
+						{children}
+					</main>
+				</div>
+			</div>
+		</div>
 	)
 }
