2a8320db5854664c5da1f1e89813f8aebf64d6f1
Nathan Gobinet <nathan.gobinet@gmail.com>
2021-11-29 21:33:20 -0500

    Add border from #282

--- src/components/Common/Layout/Layout.tsx
+++ src/components/Common/Layout/Layout.tsx
@@ -11,13 +11,13 @@ export default function Layout({ children }: LayoutSpec): JSX.Element {
 		<div className="bg-gradient-to-r from-blue-blue to-blue-light-blue min-h-screen">
 			<div className="flex items-center justify-center min-h-screen">
 				<div className={styles['container']}>
-					<div style={{ position: 'relative' }}>
-						<img
-							className={styles['logo']}
-							src="/img/logo.svg"
-							alt="Memo Draw"
-						/>
-					</div>
+					<img
+						className={styles['logo']}
+						src="/img/logo.svg"
+						alt="Memo Draw"
+					/>
+					<div className={styles['border-top-right']} />
+					<div className={styles['border-top-left']} />
 					<main className={styles['main']}>
 						{children}
 					</main>
