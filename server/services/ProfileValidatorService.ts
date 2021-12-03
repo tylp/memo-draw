c57b5c63e99dd636983c3cec392ad2b5cd58f2b7
Adrien “Nautilus” N. F <iamnautilusjungle@gmail.com>
2021-12-03 09:08:58 +0100

    catch fake profiles

--- server/services/ProfileValidatorService.ts
+++ server/services/ProfileValidatorService.ts
@@ -1,3 +1,4 @@
+import { IAvatar } from 'server/interfaces/IAvatar';
 import IProfile from '../interfaces/IProfile';
 
 const MIN_USERNAME_LENGTH = 3;
@@ -5,11 +6,16 @@ const MAX_USERNAME_LENGTH = 16;
 
 export default class ProfileValidatorService {
 	public static validate(profile: IProfile): boolean {
-		return this.validateUsername(profile?.username);
+		return this.validateUsername(profile?.username)
+			&& this.validateAvatar(profile.avatar);
 	}
 
 	public static validateUsername(username: string): boolean {
 		return username?.length >= MIN_USERNAME_LENGTH
 			&& username?.length <= MAX_USERNAME_LENGTH;
+	}
+
+	public static validateAvatar(avatar: IAvatar): boolean {
+		return !!(avatar && avatar.bodyColor && avatar.bodyType && avatar.faceType && avatar.rubberColor);
 	}
 }
