a93e19117e08eb17080dca1b565e96ef46dcb8a5
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-12-08 19:44:30 +0100

    basic draw indicator

--- src/components/Lobby/GameView/PlayerView/PlayerView.tsx
+++ src/components/Lobby/GameView/PlayerView/PlayerView.tsx
@@ -170,11 +170,21 @@ export default function PlayerView(props: PlayerViewProps): JSX.Element {
 						/>
 					</div>
 					<div className={styles['col-gap']}>
-						<div className="relative">
-							<div className="absolute top-0 md:-top-11 z-10">
+						<div className="grid grid-cols-3 justify-items-stretch">
+							<div className="justify-self-start md:-top-11 z-10">
 								<Countdown limitDate={dayjs(props.lobby.game.limitDate)} onFinish={nextDrawing} />
 							</div>
-							<div style={{ right: '0' }} className="absolute top-0 md:-top-11 z-10">
+							<div className="justify-self-stretch top-0 md:-top-11 z-10 text-center">
+								{props.currentPlayer.id === playerId && (
+									<div className="text-lg font-semibold text-white-white">
+										{props.lobby.game.currentDrawingIndex === props.lobby.game.currentNumberOfDrawings
+										&& ( <p>Draw something new to add !</p>
+										) || <p>Draw what you remember !</p> }
+										
+									</div>
+								)}
+							</div>
+							<div style={{ right: '0' }} className="justify-self-end top-0 md:-top-11 z-10">
 								<div className="bg-pink-dark-pink rounded-md px-3 py-1 text-center">
 									<span className="text-lg font-semibold text-white-white">
 										{props.lobby.game.currentDrawingIndex}/{props.lobby.game.currentNumberOfDrawings}
