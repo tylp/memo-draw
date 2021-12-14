ebcfc5c91bb358c154a2e67c71f4a7a1db8e8810
Cyril Gourgouillon <cyrilgour@gmail.com>
2021-12-14 19:23:58 +0100

    Remove T from arg

--- src/components/Lobby/GameView/PlayerView/PlayerTurnCue/PlayerTurnCue.tsx
+++ src/components/Lobby/GameView/PlayerView/PlayerTurnCue/PlayerTurnCue.tsx
@@ -1,14 +1,14 @@
-import { TFunction } from 'i18next';
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next';
 import { Lobby, Player } from '../../../../../../server/classes';
 
 export function PlayerTurnCue(props: {lobby: Lobby, playerId: Player['id']}): JSX.Element {
 	const { t } = useTranslation();
-	const [cue, setCue] = useState({color: 'text-white-white', text: ''});
+	const [cue, setCue] = useState({color: 'text-white-white', textKey: ''});
 
 	useEffect(() => {
-		setCue(CueFactory.create(props.lobby.game.currentPlayer.id === props.playerId, props.lobby.game.currentDrawingIndex === props.lobby.game.currentNumberOfDrawings, t))
+		setCue(CueFactory.create(props.lobby.game.currentPlayer.id === props.playerId,
+			props.lobby.game.currentDrawingIndex === props.lobby.game.currentNumberOfDrawings));
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [props.lobby.game.currentPlayer.id,
 		props.playerId,
@@ -17,29 +17,29 @@ export function PlayerTurnCue(props: {lobby: Lobby, playerId: Player['id']}): JS
 
 	return(
 		<div className="bg-blue-darker-blue font-bold font-rubik h-full w-auto px-4 rounded-md flex items-center">
-			<div className={cue.color}>{cue.text}</div>
+			<div className={cue.color}>{t(cue.textKey)}</div>
 		</div>
 	)
 }
 
 class CueFactory {
-	static create(isPlayerTurn: boolean, isLastDrawingIndex: boolean, t: TFunction): {color: string, text: string} {
+	static create(isPlayerTurn: boolean, isLastDrawingIndex: boolean): {color: string, textKey: string} {
 		if(isPlayerTurn) {
 			if(isLastDrawingIndex) {
 				return {
 					color: 'text-pink-light-pink',
-					text: t('gameView.drawSomethingNew'),
+					textKey: 'gameView.drawSomethingNew',
 				}
 			} else {
 				return {
 					color: 'text-yellow-light-yellow',
-					text: t('gameView.drawWhatYouRemember'),
+					textKey: 'gameView.drawWhatYouRemember',
 				}
 			}
 		} else {
 			return {
 				color: 'text-white-white',
-				text: t('gameView.memorizeEachDraw'),
+				textKey: 'gameView.memorizeEachDraw',
 			}
 		}
 	}
